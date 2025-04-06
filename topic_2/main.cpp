bbca3d13b5d92a6b82310383ded850169021251d
ttontoey <65761088+ttontoey@users.noreply.github.com>
2025-04-06 13:19:25 +0700

    feat: topic_2 analytical sol

--- topic_2/main.cpp
+++ topic_2/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
+#include <iomanip>
+
 
 using namespace std;
+
+const int RANDOM_RESOLUTION = 1e6;
 
 void print_walk_sim(int t, int pos, int bar_l, int bar_r){
     cout << "t=" << t << ": [";
@@ -15,14 +20,15 @@ void print_walk_sim(int t, int pos, int bar_l, int bar_r){
     cout << "]";
 }
 
-void rand_walk_sim(int &S_n, int &bar_l, int &bar_r, int &t, bool print){
+void rand_walk_sim(int &S_n, int &bar_l, int &bar_r, int &t, double &p, bool print){
     if (print){
         cout << endl << "### RANDOM WALK SIMULATION ###" << endl;
         print_walk_sim(0, S_n, bar_l, bar_r);
         cout << endl;
     }
     while (S_n != bar_l && S_n != bar_r){
-        int X_n = rand() % 2 == 0 ? -1 : 1;
+        double random = ((rand() % RANDOM_RESOLUTION) * 1.0)/RANDOM_RESOLUTION;
+        int X_n = random <= p ? 1 : -1;
         S_n += X_n;
         t += 1;
         if (print){
@@ -35,6 +41,7 @@ void rand_walk_sim(int &S_n, int &bar_l, int &bar_r, int &t, bool print){
 int main(){
     cout << "Simple random walk with absorption simulation" << endl;
     int a, bar_l, bar_r;
+    double p;
     cout << "Starting point: ";
     cin >> a;
     cout << "Left absorption barrier: 0" << endl;
@@ -49,6 +56,12 @@ int main(){
         cout << "Error: Right absorption barrier must be greater or equal to the starting point.";
         return 0;
     }
+    cout << "P(Moving right): ";
+    cin >> p;
+    if (p < 0 || p > 1){
+        cout << "Error: P(Moving right) must be between 0 and 1.";
+        return 0;
+    }
     int mode;
     cout << "[1] Animate [2] Monte-Carlo for Hitting Times/Probs" << endl;
     cout << "Choose mode: ";
@@ -56,7 +69,7 @@ int main(){
     int S_n = a;
     int t = 0;
     if (mode == 1){
-        rand_walk_sim(S_n, bar_l, bar_r, t, true);
+        rand_walk_sim(S_n, bar_l, bar_r, t, p, true);
     }
     else if (mode == 2){
         int sim;
@@ -67,7 +80,7 @@ int main(){
         cin >> printSim;
         int bar_l_hit = 0, bar_r_hit = 0, hitting_time_sum = 0;
         for (int i = 0 ; i < sim ; i++){
-            rand_walk_sim(S_n, bar_l, bar_r, t, printSim == 'Y');
+            rand_walk_sim(S_n, bar_l, bar_r, t, p, printSim == 'Y');
             if (S_n == bar_l)
                 bar_l_hit++;
             if (S_n == bar_r)
@@ -76,13 +89,37 @@ int main(){
             S_n = a;
             t = 0;
         }
-        double hitting_time = (hitting_time_sum * 1.0) / sim;
-        double bar_l_prob = (bar_l_hit * 1.0)/(bar_l_hit + bar_r_hit);
-        double bar_r_prob = (bar_r_hit * 1.0)/(bar_l_hit + bar_r_hit);
-        cout << endl << "### NUMERICAL RESULT ###" << endl;
-        cout << " - Expected hitting time: " << hitting_time << endl;
-        cout << " - P(Hitting the left barrier): " << bar_l_prob << endl;
-        cout << " - P(Hitting the right barrier): " << bar_r_prob << endl;
+
+        double hitting_time_a, bar_r_prob_a;
+        double q = 1 - p;
+        if (p == 0 || p == 0.5){ 
+            bar_r_prob_a = p == 0 ? 0 : (a * 1.0)/bar_r; 
+            hitting_time_a = p == 0 ? a : (a * 1.0) * (bar_r - a);
+        }
+        else {
+            bar_r_prob_a = (pow((q / p), a) - 1)/(pow((q / p), bar_r) - 1);
+            hitting_time_a = (a / (q - p)) - ((bar_r / (q - p)) * bar_r_prob_a);
+        }
+        double bar_l_prob_a = 1 - bar_r_prob_a;
+        cout << endl << "### ANALYTICAL RESULT ###" << endl;
+        cout << fixed << setprecision(5) << " - Expected hitting time: " << hitting_time_a << endl;
+        cout << fixed << setprecision(5) << " - P(Hitting the left barrier): " << bar_l_prob_a << endl;
+        cout << fixed << setprecision(5) << " - P(Hitting the right barrier): " << bar_r_prob_a << endl;
+        
+        double hitting_time_n = (hitting_time_sum * 1.0) / sim;
+        double bar_l_prob_n = (bar_l_hit * 1.0)/(bar_l_hit + bar_r_hit);
+        double bar_r_prob_n = (bar_r_hit * 1.0)/(bar_l_hit + bar_r_hit);
+        double hitting_time_err = hitting_time_a != 0 ? abs(hitting_time_n - hitting_time_a)/hitting_time_a * 100 : 0;
+        double bar_l_prob_err = bar_l_prob_a != 0 ? abs(bar_l_prob_n - bar_l_prob_a)/bar_l_prob_a * 100 : 0;
+        double bar_r_prob_err = bar_r_prob_a != 0 ? abs(bar_r_prob_n - bar_r_prob_a)/bar_r_prob_a * 100 : 0;
+
+        cout << endl << "### NUMERICAL RESULT (by Monte-Carlo) ###" << endl;
+        cout << fixed << setprecision(5) << " - Expected hitting time: " << hitting_time_n << " (";
+        cout << fixed << setprecision(2) << "Error: " << hitting_time_err << "%)" << endl;
+        cout << fixed << setprecision(5) << " - P(Hitting the left barrier): " << bar_l_prob_n << " (";
+        cout << fixed << setprecision(2) << "Error: " << bar_l_prob_err << "%)" << endl;
+        cout << fixed << setprecision(5) << " - P(Hitting the right barrier): " << bar_r_prob_n << " (";
+        cout << fixed << setprecision(2) << "Error: " << bar_r_prob_err << "%)" << endl;
     }
 
 
